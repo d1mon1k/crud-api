c4a482f4772fddc5256b76549d8f90888326e242
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-07-02 14:50:37 +0200

    chore: update prettier and eslint configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  env: { node: true },
+  rules: { 'no-console': 'warn' },
   root: true,
 };
