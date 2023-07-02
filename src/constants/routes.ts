ffe7e5f86968874262bef73db617265d6ccf7edc
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-07-02 13:59:14 +0200

    feat: put different constants in variables

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export enum USERS_ROUTES {
+  GET_USERS = 'api/users',
+  GET_USER_BY_ID = 'api/users/{userId}',
+  ADD_USER = 'api/users',
+  UPDATE_USER_BY_ID = 'api/users/{userId}',
+  DELETE_USER = 'api/users/{userId}',
+}
